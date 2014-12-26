7eae1645942d046f193b4b847a1ec0ab0e4a8a39
haffmaestro <halfdanhem@gmail.com>
2014-12-26 17:41:49 -0600

    Removed old status method from UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,17 +4,9 @@ class UsersController < ApplicationController
 
   def index
     @users = User.without_user(current_user)
-
     respond_to do |format|
       format.html {@users}
       format.json {render json: @users, each_serializer: UserSerializer}
-    end
-  end
-
-  def status
-    @user = User.find(params[:id])
-    respond_to do |format|
-      format.json { render :json =>  @user.to_json(:only => [:id, :status], :methods => [:full_name]) }
     end
   end
 
@@ -40,7 +32,6 @@ class UsersController < ApplicationController
 
   def events
     response.headers["Content-Type"] = "text/event-stream"
-
     redis = Redis.new
     redis.psubscribe('users.*') do |on|
       on.pmessage do |pattern, event, data|
